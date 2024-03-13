8e265c7f5f7c9abbc4b089f7f3610d8379874b33
jaminleee <jamin_00@naver.com>
2024-03-13 01:33:20 -0500

    Initial Commit

---
+++ UNIV-4cut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UNIV-4cut
+//
+//  Created by 이자민 on 3/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UNIV-4cut/UNIV_4cutApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNIV_4cutApp.swift
+//  UNIV-4cut
+//
+//  Created by 이자민 on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct UNIV_4cutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
