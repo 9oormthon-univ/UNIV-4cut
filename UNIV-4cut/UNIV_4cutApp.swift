8279a19912de1ba557dc0dfe3e634650d5ae340c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-03-16 03:10:57 +0900

    :sparkles: Feat: firebase setting
    feat: firebase setting

--- UNIV-4cut/UNIV_4cutApp.swift
+++ UNIV-4cut/UNIV_4cutApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import Firebase
 
 @main
 struct UNIV_4cutApp: App {
+    // 파베 초기화
+    init() {
+         FirebaseApp.configure()
+     }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
