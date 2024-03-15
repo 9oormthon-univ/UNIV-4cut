19cb8828f5e153eb3def53b1e181e4f6cd874a76
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-03-16 03:10:00 +0900

    :sparkles: Feat: imageProcessor(Merge Func)
    feat: imageProcessor(Merge Func)

--- UNIV-4cut/Code/Utilities/Result/ImageProcessor.swift
+++ UNIV-4cut/Code/Utilities/Result/ImageProcessor.swift
@@ -1,8 +1,22 @@
-//
-//  ImageProcessor.swift
-//  UNIV-4cut
-//
-//  Created by 서희찬 on 3/16/24.
-//
+import UIKit
 
-import Foundation
+class ImageProcessor {
+    // 이미지 두 개 합치기
+    func mergeImage(image: UIImage) -> UIImage? {
+        guard let frameImage = UIImage(named: "4cut_frame") else {
+            print("프레임 이미지를 불러올 수 없습니다.")
+            return nil
+        }
+
+        let size = frameImage.size
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+
+        image.draw(in: CGRect(x: 0, y: 0, width: size.width, height: size.height-130))
+        frameImage.draw(in: CGRect(x: 0, y: 0, width: size.width + 5, height: size.height))
+
+        let mergedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return mergedImage
+    }
+}
